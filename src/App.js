6837afa6ecf9cd2d4719facad910ee3ae6d76b4b
Alex Vestelind <alex_vestelind@hotmail.com>
2023-02-02 12:58:43 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Game from './Components/Game';
 
 function App() {
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,10 +1,15 @@
 import './Game.css'
 import React from 'react';
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Game = () => {
 const [player, setPlayer] = useState('red')
 const [moves, setMoves] = useState([])
+const [winner, setWinner] = useState('')
+const [winMoves, setWinMoves] = useState([])
+const rows = 6;
+const columns = 7;
 
 const getPiece = (x, y) => {
     const list = moves.filter((item) => {
@@ -15,18 +20,73 @@ const getPiece = (x, y) => {
 
 const resetBoard = () => {
     setMoves([])
+    setWinner('')
+    setWinMoves([])
+}
+
+const getWinningMovesDiagonal = (xPosition, yPosition, xVelocity, yVelocity) => {
+    let winningMoves = [{x: xPosition, y: yPosition}]
+    for(let delta = 1; delta <= 3; delta++){
+        const checkX = xPosition + xVelocity * delta;
+        const checkY = yPosition + yVelocity * delta;
+        const checkPiece = getPiece(checkX, checkY)
+        if(checkPiece && checkPiece.player === player)
+        {
+            winningMoves.push({x: checkX, y: checkY})
+        } else{
+            break;
+        }
+    }
+    for(let delta = -1; delta >= -3; delta--){
+        const checkX = xPosition + xVelocity * delta;
+        const checkY = yPosition + yVelocity * delta;
+        const checkPiece = getPiece(checkX, checkY)
+        if(checkPiece && checkPiece.player === player)
+        {
+            winningMoves.push({x: checkX, y: checkY})
+        } else{
+            break;
+        }
+    }
+
+    return winningMoves;
+}
+
+const checkForWin = (x, y) => {
+    const velocities = [{x:1,y:0}, {x:0,y:1}, {x:-1, y:1}, {x:1,y:1}]
+    for(let i = 0; i<velocities.length;i++){
+        const element = velocities[i]
+        const winningMoves = getWinningMovesDiagonal(x,y,element.x,element.y)
+        if(winningMoves.length > 3){
+            setWinner(player)
+            setWinMoves(winningMoves)
+        }
+    }
 }
 
 const addMove = (x, y) => {
     const nextPlayer = player === 'red' ? 'yellow' : 'red';
-    const move = moves.concat({x, y, player})
-    setMoves(move)
-    setPlayer(nextPlayer)
+    let avaliableYposition = null;
+    moves.concat([])
+    for(let position = rows -1; position >= 0; position--){
+        if(!getPiece(x, position)){
+            avaliableYposition = position;
+            break;
+        }
+    }
+    if(avaliableYposition !== null){
+        const move = moves.concat({x,y: avaliableYposition,player: player})
+        setMoves(move)
+        checkForWin(x, avaliableYposition, player)
+        setPlayer(nextPlayer) 
+        
+    }
+
+      
 }
 
 const CreateBoard = () => {
-    const rows = 6;
-    const columns = 7;
+
 
     let rowViews = [];
     for(let row = 0; row < rows; row++){
@@ -34,7 +94,7 @@ const CreateBoard = () => {
         for(let column = 0; column < columns; column++){
             const piece = getPiece(column, row)
             columnViews.push(
-            <div onClick={() => {addMove(column, row)}} style={{cursor: 'pointer', width: '8vw', height: '8vw', backgroundColor: '#00a8ff', display: 'flex', padding: 5}}>
+            <div onClick={() => {addMove(column, row)}} style={{cursor: 'pointer', width: '6vw', height: '6vw', backgroundColor: '#00a8ff', display: 'flex', padding: 5}}>
                 <div style={{borderRadius: '50%', backgroundColor: 'white', flex: 1, display: 'flex'}}>
                     {piece ? <div style={{backgroundColor: piece.player, flex: 1, borderRadius: '50%', border: '1px solid black'}} /> : undefined}
                 </div>
@@ -50,9 +110,12 @@ const CreateBoard = () => {
     return(
         <div>
         <div style={{backgroundColor: 'blue', display: 'flex', flexDirection: 'column'}}>
+            {winner && <div onClick={resetBoard} style={{position: 'absolute',left:0,right:0,bottom:0,top:0, backgroundColor: 'rgba(0,0,0,.5)',
+            zIndex: 3, display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontSize: '5vw'}}>{winner.toUpperCase()} WINS</div>}
             {rowViews}
         </div>
-        <button onClick={resetBoard}>reset Game</button>
+        <button onClick={resetBoard} className='me-4 btn btn-danger btn-lg btn-block'>RESET</button>
+        <h3 style={{color: player, float: 'right'}}>{player}'s turn</h3>
         </div>
     );
 }
