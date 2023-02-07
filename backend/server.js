d523dbbd9b3c827377799572c7379135858d4300
Alex Vestelind <alex_vestelind@hotmail.com>
2023-02-07 09:55:28 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,74 +4,47 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const MoveModel = require("../src/Models/moveModel")
 // const PORT = 3001;
-// const userRoutes = require("../src/Routes/moveRoute")
+const userRoutes = require("../src/Routes/moveRoute")
 
 var bodyParser = require('body-parser')
 
-const uri = "mongodb+srv://bombsoldier:<password>@cluster0.ld5n3bl.mongodb.net/connect?retryWrites=true&w=majority";
-const mongo_uri = "mongodb+srv://bombsoldier:<password>@cluster0.ld5n3bl.mongodb.net/connect?retryWrites=true&w=majority"
+const uri = "mongodb+srv://bombsoldier:password321@alex-connect-db.iuafl4l.mongodb.net/connect4?retryWrites=true&w=majority";
 
-app.use(cors());
-app.use(express.json());
+
+app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
 
 
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, monitorCommands: true });
-// client.connect(err => {
-//   const collection = client.db("connect").collection("moves");
 
+//connect to db
+mongoose.set('strictQuery', true);
+mongoose.connect(uri,{
+    family: 4
+},
+(err) => {
+    if(err) console.log(err.reason)
+    else console.log("mongodb is connected")
+    }
+);
 
-//     if(err) console.log(err.reason)
-//     else console.log("mongodb connected successfully")
-
- 
-  
-// });
-async function start(){
-try{await mongoose.connect(mongo_uri, {}, 6000000)
-.then(() => console.log("database connected"))
-.catch(err => console.log(err))
-}catch(err){
-    console.log(err)
-}
-}
-start()
-app.get("/getMoves", (req, res) => {
-    MoveModel.find({}, (err, result) => {
-        if(err){
-            res.json(err)
-        } else{
-            res.json(result)
+//tried routing here instead of a separate file, ignore moverouter file if this route is not commented out
+app.post("/create", function(req, res){
+    var move = req.body;
+    console.log(move)
+    var newMove = new MoveModel({
+        "x": move.x,
+        "y": move.y,
+        "player": move.player
+    });
+    console.log(newMove)
+    newMove.save((err) => {
+        if (err) {
+          console.error(err);
         }
     })
-})
-app.post("/create", async (req, res) => {
-    const move = req.body;
-    const newMove = new MoveModel(move);
-    console.log(newMove)
-    await newMove.save();
-
     res.json(move)
 })
-// mongoose.set('strictQuery', true);
-// mongoose.connect(uri,
-// {
-//     wtimeoutMS: 0,
-//     ssl: true,
-//     sslValidate: true,
-//     dbName: 'connect',
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     family: 4,
-//     keepAlive: true,
-//     keepAliveInitialDelay: 300000,
-// },
-// (err) => {
-//     if(err) console.log(err.reason)
-//     else console.log("mongodb is connected")
-//     }
-// );
+
 
 
 
