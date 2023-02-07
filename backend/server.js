4f25f694460cff4ce1a1fbee9d6adc9f55d13cfe
Alex Vestelind <alex_vestelind@hotmail.com>
2023-02-07 12:11:02 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
-const MoveModel = require("../src/Models/MoveModel")
+
 // const PORT = 3001;
 const userRoutes = require("../src/Routes/MoveRoute")
 
@@ -14,11 +14,19 @@ const uri = "mongodb+srv://bombsoldier:password321@alex-connect-db.iuafl4l.mongo
 app.use(cors())
 app.use(bodyParser.json())
 
+const moveSchema = new mongoose.Schema({
+    x: Number,
+    y: Number,
+    player: String
+})
 
+const Move = mongoose.model('Move', moveSchema)
 
 //connect to db
+async function StartUp(){
+    try{
 mongoose.set('strictQuery', true);
-mongoose.connect(uri,{
+await mongoose.connect(uri,{
     family: 4
 },
 (err) => {
@@ -27,24 +35,49 @@ mongoose.connect(uri,{
     }
 );
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+})
+}catch (err) 
+{console.log(err)}}
+StartUp()
+
+
+
 //tried routing here instead of a separate file, ignore moverouter file if this route is not commented out
-app.post("/create", function(req, res){
-    var move = req.body;
-    console.log(move)
-    var newMove = new MoveModel({
-        "x": move.x,
-        "y": move.y,
-        "player": move.player
+app.post("/create", async function(req, res){
+    var {x, y, player} = req.body;
+    console.log(x, y, player)
+    var newMove = new Move({
+        x: x,
+        y: y,
+        player: player
     });
     console.log(newMove)
-    newMove.save((err) => {
+    await newMove.save((err) => {
         if (err) {
           console.error(err);
         }
     })
-    res.json(move)
+    res.json(x, y, player)
 })
 
+app.get("/getAll", async function(req, res){
+    try{
+    const filter = {}
+    const data = await Move.find()
+    res.send(data)
+    }
+    catch (err) {console.log(err)}
+})
+
+app.post("/reset", async function(req, res){
+    try{
+    await Move.deleteMany()
+    }catch(err){console.log(err)}
+})
 
 
 
